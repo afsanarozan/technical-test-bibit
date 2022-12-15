46ca560b9c38139ef8622510bd7571f20a14672e
afsanarozan <68263390+afsanarozan@users.noreply.github.com>
2022-12-15 16:55:49 +0700

    Update run.sh

--- terraform-test/run.sh
+++ terraform-test/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-  sudo mkdir /mnt/diskb/ || true
   sudo systemctl stop unattended-upgrades.service
   sudo apt update && sudo apt install jq -y 
   
