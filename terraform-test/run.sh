229e9b11286cdc71d24450e48d33ed539135a364
afsanarozan <68263390+afsanarozan@users.noreply.github.com>
2022-12-15 16:55:28 +0700

    Update run.sh

--- terraform-test/run.sh
+++ terraform-test/run.sh
@@ -3,17 +3,20 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
   sudo mkdir /mnt/diskb/ || true
   sudo systemctl stop unattended-upgrades.service
   sudo apt update && sudo apt install jq -y 
-
-  #check volume
-  sudo mount 10.0.6.153:/mnt/diskb /mnt/diskb || true
+  
+  #install fluenbit 
+  sudo apt-get install fluent-bit
 
   #start fluent-bit 
   sudo systemctl start fluent-bit
+  
+  #install nginx
+  sudp apt install nginx
 
   #start nginx
   sudo systemctl start nginx 
-
-
+  
+  ### install & configure will be execute after instance spawn
 
   #run docker-compose
 #   cd /home/ubuntu/
@@ -22,6 +25,6 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 #   sudo docker run -itd --name="node-exporter-dev" --net="host" --pid="host" -v "/:/host:ro,rslave" quay.io/prometheus/node-exporter --path.rootfs=/host
 
   #install teleport-agent
-  aws s3 cp s3://teleport-master-log/teleport-install9.3.0custom.sh . && 
-  chmod +x ./teleport-install9.3.0custom.sh &&
-  sudo bash ./teleport-install9.3.0custom.sh
+#   aws s3 cp s3://teleport-master-log/teleport-install9.3.0custom.sh . && 
+#   chmod +x ./teleport-install9.3.0custom.sh &&
+#   sudo bash ./teleport-install9.3.0custom.sh
